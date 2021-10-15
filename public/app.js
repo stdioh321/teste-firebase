b6810b877c8a2e20ca047bc6f77bef73a96ac3b2
diaslinoh <diaslinoh@gmail.com>
2021-10-15 02:49:04 -0300

    Remove document.

--- public/app.js
+++ public/app.js
@@ -2,11 +2,12 @@ document.addEventListener("DOMContentLoaded", event => {
 
     const app = firebase.app();
 
-    document.querySelector("#inpUpdatePost").addEventListener("input", _.debounce(onUpdatePost, 200));
+    document.querySelector("#inpUpdatePost").addEventListener("input", _.debounce(onUpdatePost, 300));
 
     firestoreNormal();
     firestoreRealtime();
-
+    firestoreAll();
+    firestoreAllRemove();
 
 });
 
@@ -16,6 +17,11 @@ function getDoc(collection = "posts", doc = "mypost") {
     return res;
 }
 
+function onDeleteDoc(id) {
+    const ok = confirm("Are you sure???");
+    if (!ok) return;
+    getDoc("posts", id).delete();
+}
 async function onUpdatePost(ev) {
     const value = ev.target.value;
     const myPost = getDoc();
@@ -25,6 +31,78 @@ async function onUpdatePost(ev) {
     } catch (error) {
         console.log(error);
     }
+}
+let addPostLoading = false;
+async function onAddPost(self) {
+    if (addPostLoading) return;
+    [].forEach.call(self.elements, it => it.disabled = true);
+    addPostLoading = true;
+
+    const value = self.elements[0].value;
+    const db = firebase.firestore();
+    const collection = db.collection("posts");
+    try {
+        const doc = await collection.add({ title: value });
+        console.log("Doc added");
+    } catch (error) {
+        console.log(error);
+    }
+    [].forEach.call(self.elements, it => it.disabled = false);
+    addPostLoading = false;
+}
+
+function firestoreAll() {
+    const all = document.querySelector("#firestoreAll");
+    const db = firebase.firestore();
+    const docs = db.collection("posts");
+    docs.onSnapshot(querySnapshot => {
+        all.innerHTML = "";
+        querySnapshot.docs.forEach(it => {
+
+            const json = JSON.stringify(it.data(), null, 2);
+            all.innerHTML += `<pre class="text-warning small">id: ${it.id} - ${json}</pre>`
+        })
+    });
+
+    // docs.get()
+    //     .then(querySnapshot => {
+    //         all.innerHTML = "";
+    //         querySnapshot.docs.forEach(it => {
+
+    //             const json = JSON.stringify(it.data(), null, 2);
+    //             all.innerHTML += `<pre class="text-warning small">id: ${it.id} - ${json}</pre>`
+    //         })
+    //     })
+    //     .catch(err => {
+    //         console.log(err);
+    //     });
+}
+
+function firestoreAllRemove() {
+    const all = document.querySelector("#firestoreRemove");
+    const db = firebase.firestore();
+    const docs = db.collection("posts");
+    docs.where(firebase.firestore.FieldPath.documentId(), "!=", "mypost").onSnapshot(querySnapshot => {
+        all.innerHTML = "<ul class='list-group'>";
+        querySnapshot.docs.forEach(it => {
+            const data = it.data();
+            all.innerHTML += `<li class="list-group-item d-flex align-items-center"><div class="flex-fill" ><div>id: ${it.id}</div><small class="text-muted">title: ${data.title}</small></div><div><button  onclick="onDeleteDoc('${it.id}')" class="btn btn-danger">X</button></div></li>`
+        })
+        all.innerHTML += "</ul";
+    });
+
+    // docs.get()
+    //     .then(querySnapshot => {
+    //         all.innerHTML = "";
+    //         querySnapshot.docs.forEach(it => {
+
+    //             const json = JSON.stringify(it.data(), null, 2);
+    //             all.innerHTML += `<pre class="text-warning small">id: ${it.id} - ${json}</pre>`
+    //         })
+    //     })
+    //     .catch(err => {
+    //         console.log(err);
+    //     });
 }
 
 function firestoreNormal() {
