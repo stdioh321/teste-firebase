e82523ee435fd75e48e7e1085ec25d55d19de2bb
diaslinoh <diaslinoh@gmail.com>
2021-10-15 00:28:58 -0300

    [DONE] LoginWithGoogle;

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # teste-firebase
 Testing Firebase
+
+
+
+
+
+
+# References
+* https://www.youtube.com/watch?v=9kRgVxULbag
---
+++ public/app.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", event => {
+    const app = firebase.app();
+    console.log(app);
+});
+
+let loginWithGoogle = false;
+// GLOBAL
+async function loginWithGoogle(self) {
+    if (loginWithGoogle) return;
+    loginWithGoogle = true;
+    self.disabled = true;
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const auth = firebase.auth();
+
+    try {
+        const res = await auth.signInWithPopup(provider);
+        const { user } = res;
+        document.querySelector("#loginWithGoogleRes").innerHTML = `Hello: ${user.displayName}`;
+        // const res = await auth.signInWithRedirect(provider);
+        console.log(res);
+    } catch (error) {
+        console.log("%c ERROR", "color:red; font-size:2em");
+        console.log(error);
+    }
+    self.disabled = false;
+    loginWithGoogle = false;
+}
